2965e4b0ec2e2b0bf3bdbc4b91aa90787a1e750a
Bruno Carneiro <b.h.r.carneiro@gmail.com>
2024-01-04 09:04:52 +0000

    fix issues of deploy process

--- m3-rest-api/project-management-server/app.js
+++ m3-rest-api/project-management-server/app.js
@@ -1,11 +1,5 @@
 require("dotenv").config();
 
-const OpenAI = require('openai');
-const { apiKey } = process.env.API_KEY;
-const openai = new OpenAI({
-  apiKey: "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",
-});
-
 const cors = require("cors");
 const bodyParser = require("body-parser");
 require("./db");
