ff53747483c493afff4e5e9d39870459027c22c6
Bruno Carneiro <b.h.r.carneiro@gmail.com>
2024-01-10 10:44:16 +0000

    correct ORIGIN

--- m3-rest-api/project-management-server/app.js
+++ m3-rest-api/project-management-server/app.js
@@ -1,18 +1,15 @@
+const cors = require("cors");
 require("dotenv").config();
 
-const cors = require("cors");
-const bodyParser = require("body-parser");
 require("./db");
 
 const express = require("express");
 
 const app = express();
 
-app.use(bodyParser.json());
-
 app.use(
     cors({
-      origin: "*",
+      origin: ["https://fabulous-gnome-6f4332.netlify.app", "http://localhost:5173"],
     })
   );
 
